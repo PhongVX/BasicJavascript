460985437326d131c95843cb646d7045993e73f3
Phong Vo <phongvxk38@gmail.com>
2021-04-01 06:14:02 +0700

    Update GET, POST, PUT, DELETE

--- simple-service/routes/users.js
+++ simple-service/routes/users.js
@@ -14,10 +14,32 @@ router.get('/:userId/skills', (req, res) => {
 
 router.post('/', (req, res) => {
     const body = req.body;
-    console.log(body);
     const newUser = {id: new Date().getTime(), firstName: body.firstName, lastName: body.lastName, sex: body.sex };
     users.push(newUser);
     res.json({result: newUser});
+});
+
+router.put('/', (req, res) => {
+    const body = req.body;
+    const { id, ...restInfo } = body;
+    let updatedUser = {};
+    for (let user of users){ 
+        if (user.id === id) { 
+            updatedUser = {...user, ...restInfo};
+            user.firstName = updatedUser.firstName;
+            user.lastName  = updatedUser.lastName;
+            user.sex = updatedUser.sex;
+            break;
+        }
+    }
+    res.json({result: updatedUser});
+});
+
+router.delete('/:id', (req, res) => { 
+    const { id } = req.params;
+    const userIndex = users.findIndex(u => u.id !== id);
+    users.splice(userIndex, 1);
+    res.json({result: users});
 });
 
 module.exports = router;
